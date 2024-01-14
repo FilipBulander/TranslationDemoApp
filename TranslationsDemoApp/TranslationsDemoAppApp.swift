0c5f9e20a23fd1f5d46ebf2b5d6e452bfc7ebbc2
Filip Bulander <filip.bulander@gmail.com>
2024-01-14 14:29:47 +0100

    Initial Commit

---
+++ TranslationsDemoApp/TranslationsDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationsDemoAppApp.swift
+//  TranslationsDemoApp
+//
+//  Created by Filip Bulander on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TranslationsDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
